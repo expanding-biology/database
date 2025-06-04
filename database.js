2d49c9d16ca50fde20ba2e6a3107519b301bf313
expanding-biology <expanding.biology@gmail.com>
2025-06-04 18:43:54 +0200

    Create database.js

---
+++ database.js
@@ -0,0 +1,11 @@
+const database = [
+  {
+    name: "MAGNIFY",
+    authors: "Aleksandra Klimas, Brendan R. Gallagher, Piyumi Wijesekara, Sinda Fekir, Emma F. DiBernardo, Zhangyu Cheng, Donna B. Stolz, Franca Cambi, Simon C. Watkins, Steven L. Brody, Amjad Horani, Alison L. Barth, Christopher I. Moore, Xi Ren, Yongxin Zhao",
+    sample: "tissue slices",
+    organism: "mouse",
+    resolution: "10–50 nm",
+    biomolecules: "proteins",
+    link: "https://www.nature.com/articles/s41587-022-01546-1"
+  },
+];
